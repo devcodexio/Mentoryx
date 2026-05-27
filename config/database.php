aff9f7f7c04e2843995d61e248b53687ca2dc83e
Miguel <miguelangel01032001@gmail.com>
2026-05-27 16:49:43 -0500

    Actualización del sistema

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'host' => '127.0.0.1',
-    'db' => 'simulador_examenes',
-    'user' => 'root',
-    'pass' => '',
+    'host' => 'localhost',
+    'db' => 'angeloay_startraining',
+    'user' => 'angeloay_startraining',
+    'pass' => 'TU_CONTRASEÑA_AQUI',
     'charset' => 'utf8mb4'
 ];
